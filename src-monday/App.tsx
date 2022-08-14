3a506d66707e6e4784e4a393c9c1ef852b97093b
Vladimir Zvyagin <vladimir_zvyagin@mail.ru>
2022-08-15 00:33:52 +0300

    Tuesday_task07_complete

---
+++ src-monday/App.tsx
@@ -0,0 +1,181 @@
+import React, {useState} from 'react';
+import './App.css';
+import {Header} from './monday-task1/header'
+import {Body} from "./monday-task1/body";
+import {Footer} from "./monday-task1/footer";
+import {CarsTable, NewComponent} from "./monday-task1/Map";
+import {Task} from "./monday-task1/TaskNumber";
+import {students} from "./monday-task1/Map";
+import {topCars} from "./monday-task1/Map";
+import {Button} from "./monday-task1/button";
+import {Money} from "./monday-task1/money";
+import {Fullinput} from "./monday-task1/Task_6_input/Fullinput";
+import {Input} from "./monday-task1/Task_6_input/Input";
+import {ButtonInput} from "./monday-task1/Task_6_input/ButtonInput";
+import {FullMyselfInput} from "./monday-task1/Task_6_input/Inputs_myself/Full_myselfInput";
+import {ButtonInputMyself} from "./monday-task1/Task_6_input/Inputs_myself/ButtonInput-myself";
+import {InputMyself} from "./monday-task1/Task_6_input/Inputs_myself/InputMyself";
+
+
+function App() {
+
+    const Button1foo = (subscriber: string, age: number) => {
+        console.log(subscriber, age)
+    }
+    const Button2foo = (subscriber: string, age: number) => {
+        console.log(subscriber, age)
+    }
+    const Button3foo = () => {
+        console.log('Who are stupid Buttons?')
+    }
+
+    //хуки
+    let [a, setA] = useState(0)
+    const counterButton = () => {
+        setA(++a);
+        console.log(a)
+    }
+    const resetCounterButton = () => {
+        setA(0);
+        console.log(a)
+    }
+
+    //метод FILTER
+    const money = [
+        {banknots: 'Dollars', value: 100, number: ' a1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' z1234567890'},
+        {banknots: 'RUBLS', value: 100, number: ' w1234567890'},
+        {banknots: 'Dollars', value: 100, number: ' e1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' c1234567890'},
+        {banknots: 'RUBLS', value: 100, number: ' r1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' x1234567890'},
+        {banknots: 'RUBLS', value: 50, number: ' v1234567890'},
+    ]
+
+    /* let [filteredMoney, setFmoney] = useState(money);
+
+     const moneyFilterHandler = (filt: string) => {
+         if (filt === 'allMoney') {filteredMoney = money.filter(el=>{
+             return true
+         })};
+         if (filt === 'RUBLS') {filteredMoney = money.filter(el=>{
+             return el.banknots === 'RUBLS'
+         })};
+         if (filt === 'Dollars') {filteredMoney = money.filter(el=>{
+             return el.banknots === 'Dollars'
+         })};
+         setFmoney(filteredMoney)
+         console.log(filt)
+     }*/
+
+    //альтернативный код для FILTER с хуками
+    let filteredMoney = money;
+    const [filt, filtFunc] = useState('allMoney')
+
+    if (filt === 'RUBLS') {
+        filteredMoney = money.filter(el => {
+            return el.banknots === 'RUBLS'
+        })
+    }
+
+
+    if (filt === 'Dollars') {
+        filteredMoney = money.filter(el => {
+            return el.banknots === 'Dollars'
+        })
+    }
+
+
+
+    const moneyFilterHandler = (filt: string) => {
+        filtFunc(filt)
+        console.log(filt)
+    }
+
+    // INPUT
+
+    // Fullinput
+    let [message, setMessage] = useState([
+        {message: 'message1'},
+        {message: 'message2'},
+        {message: 'message3'},
+    ])
+
+    const addMessage = (message: string) => {
+        setMessage((messages) => [{message: message}, ...messages])
+    }
+
+    //INPUT + BUTTON
+    let [message1, setMessage1] = useState([
+        {message: 'inp+but-1'},
+        {message: 'inp+but-2'},
+        {message: 'inp+but-3'},
+    ])
+
+    let [title1, setTitle1] = useState<string>('')
+
+    const addMessage1 = () => {
+        setMessage1([{message: title1}, ...message1])
+        setTitle1('')
+    }
+
+    return (
+        <>
+
+            <>
+                <Task day={'MONDAY'} task={1}/>
+                <Header titleHeader={'Header-very good'}/>
+                <Body titleBody={'Body-very well'}/>
+                <Footer titleFooter={'Footer-it is beautiful'}/>
+            </>
+            <br/>
+            <>
+                <Task day={'MONDAY'} task={'2-3'}/>
+                <NewComponent students={students}/>
+                <br/>
+                <CarsTable topCars={topCars}/>
+                <Button name={'MyYouTubeChannel-1'} callback={() => Button1foo('I m Vasya', 21)}/>
+                <Button name={'MyYouTubeChannel-2'} callback={() => Button2foo('I m Ivan', 30)}/>
+                <Button name={'Stupid Button'} callback={Button3foo}/>
+            </>
+            <br/>
+            <br/>
+            <>
+                <Task day={'MONDAY'} task={4}/>
+                <h1>{a}</h1>
+                <Button name={'УВЕЛИЧИТЬ ЧИСЛО'} callback={counterButton}></Button>
+                <Button name={'ОБНУЛИТЬ СЧЕТЧИК'} callback={resetCounterButton}></Button>
+            </>
+            <br/>
+            <br/>
+            <>
+                <Task day={'MONDAY'} task={5}/>
+                <h3>МЕТОД FILTER</h3>
+                <Money money={filteredMoney}/>
+                <div style={{marginLeft: '60px'}}>
+                    <Button name={'allMoney'} callback={() => moneyFilterHandler('allMoney')}></Button>
+                    <Button name={'RUBLS'} callback={() => moneyFilterHandler('RUBLS')}></Button>
+                    <Button name={'Dollars'} callback={() => moneyFilterHandler('Dollars')}></Button>
+                </div>
+                <br/>
+            </>
+            <>
+                <Task day={'MONDAY'} task={6}/>
+                <h3>INPUT</h3>
+                <h3>FULLINPUT</h3>
+                <Fullinput addMessage={addMessage}/>
+                {message.map((m, i) => <div key={i + 1}>{m.message}</div>)}
+                <br/>
+                <h3>INPUT + BUTTON</h3>
+                <Input title={title1} setTitle={setTitle1}/>
+                <ButtonInput name={'+'} callback={addMessage1}/>
+                {message1.map((m, i) => <div key={i + 1}>{m.message}</div>)}
+
+
+            </>
+
+        </>
+    );
+}
+
+export default App;
--- src/App.tsx
+++ src/App.tsx
@@ -1,180 +1,109 @@
 import React, {useState} from 'react';
 import './App.css';
-import {Header} from './monday-task1/header'
-import {Body} from "./monday-task1/body";
-import {Footer} from "./monday-task1/footer";
-import {CarsTable, NewComponent} from "./monday-task1/Map";
-import {Task} from "./monday-task1/TaskNumber";
-import {students} from "./monday-task1/Map";
-import {topCars} from "./monday-task1/Map";
-import {Button} from "./monday-task1/button";
-import {Money} from "./monday-task1/money";
-import {Fullinput} from "./monday-task1/Task_6_input/Fullinput";
-import {Input} from "./monday-task1/Task_6_input/Input";
-import {ButtonInput} from "./monday-task1/Task_6_input/ButtonInput";
-import {FullMyselfInput} from "./monday-task1/Task_6_input/Inputs_myself/Full_myselfInput";
-import {ButtonInputMyself} from "./monday-task1/Task_6_input/Inputs_myself/ButtonInput-myself";
-import {InputMyself} from "./monday-task1/Task_6_input/Inputs_myself/InputMyself";
+import {Todolist} from './Todolist';
+import {v1} from 'uuid';
 
+
+export type todolistsType = {id: string, title: string, filter: FilterValuesType}
+
+export type FilterValuesType = "all" | "active" | "completed";
 
 function App() {
 
-    const Button1foo = (subscriber: string, age: number) => {
-        console.log(subscriber, age)
-    }
-    const Button2foo = (subscriber: string, age: number) => {
-        console.log(subscriber, age)
-    }
-    const Button3foo = () => {
-        console.log('Who are stupid Buttons?')
-    }
+    // let [tasks, setTasks] = useState([
+    //     {id: v1(), title: "HTML&CSS", isDone: true},
+    //     {id: v1(), title: "JS", isDone: true},
+    //     {id: v1(), title: "ReactJS", isDone: false},
+    //     {id: v1(), title: "Rest API", isDone: false},
+    //     {id: v1(), title: "GraphQL", isDone: false},
+    // ]);
+    // let [filter, setFilter] = useState<FilterValuesType>("all");
 
-    //хуки
-    let [a, setA] = useState(0)
-    const counterButton = () => {
-        setA(++a);
-        console.log(a)
-    }
-    const resetCounterButton = () => {
-        setA(0);
-        console.log(a)
-    }
+    let todolistID1=v1();
+    let todolistID2=v1();
 
-    //метод FILTER
-    const money = [
-        {banknots: 'Dollars', value: 100, number: ' a1234567890'},
-        {banknots: 'Dollars', value: 50, number: ' z1234567890'},
-        {banknots: 'RUBLS', value: 100, number: ' w1234567890'},
-        {banknots: 'Dollars', value: 100, number: ' e1234567890'},
-        {banknots: 'Dollars', value: 50, number: ' c1234567890'},
-        {banknots: 'RUBLS', value: 100, number: ' r1234567890'},
-        {banknots: 'Dollars', value: 50, number: ' x1234567890'},
-        {banknots: 'RUBLS', value: 50, number: ' v1234567890'},
-    ]
-
-    /* let [filteredMoney, setFmoney] = useState(money);
-
-     const moneyFilterHandler = (filt: string) => {
-         if (filt === 'allMoney') {filteredMoney = money.filter(el=>{
-             return true
-         })};
-         if (filt === 'RUBLS') {filteredMoney = money.filter(el=>{
-             return el.banknots === 'RUBLS'
-         })};
-         if (filt === 'Dollars') {filteredMoney = money.filter(el=>{
-             return el.banknots === 'Dollars'
-         })};
-         setFmoney(filteredMoney)
-         console.log(filt)
-     }*/
-
-    //альтернативный код для FILTER с хуками
-    let filteredMoney = money;
-    const [filt, filtFunc] = useState('allMoney')
-
-    if (filt === 'RUBLS') {
-        filteredMoney = money.filter(el => {
-            return el.banknots === 'RUBLS'
-        })
-    }
-
-
-    if (filt === 'Dollars') {
-        filteredMoney = money.filter(el => {
-            return el.banknots === 'Dollars'
-        })
-    }
-
-
-
-    const moneyFilterHandler = (filt: string) => {
-        filtFunc(filt)
-        console.log(filt)
-    }
-
-    // INPUT
-
-    // Fullinput
-    let [message, setMessage] = useState([
-        {message: 'message1'},
-        {message: 'message2'},
-        {message: 'message3'},
+    let [todolists, setTodolists] = useState<Array<todolistsType>>([
+        {id: todolistID1, title: 'What to learn', filter: 'all'},
+        {id: todolistID2, title: 'What to buy', filter: 'all'},
     ])
 
-    const addMessage = (message: string) => {
-        setMessage((messages) => [{message: message}, ...messages])
+    let [tasks, setTasks] = useState({
+        [todolistID1]:[
+            {id: v1(), title: "HTML&CSS", isDone: true},
+            {id: v1(), title: "JS", isDone: true},
+            {id: v1(), title: "ReactJS", isDone: false},
+            {id: v1(), title: "Rest API", isDone: false},
+            {id: v1(), title: "GraphQL", isDone: false},
+        ],
+        [todolistID2]:[
+            {id: v1(), title: "HTML&CSS2", isDone: true},
+            {id: v1(), title: "JS2", isDone: true},
+            {id: v1(), title: "ReactJS2", isDone: false},
+            {id: v1(), title: "Rest API2", isDone: false},
+            {id: v1(), title: "GraphQL2", isDone: false},
+        ]
+    });
+
+
+
+    function removeTask(todoId: string, taskId: string) {
+        setTasks({...tasks, [todoId]: tasks[todoId].filter(t => t.id !== taskId)});
     }
 
-    //INPUT + BUTTON
-    let [message1, setMessage1] = useState([
-        {message: 'inp+but-1'},
-        {message: 'inp+but-2'},
-        {message: 'inp+but-3'},
-    ])
+    function addTask(todoId: string, title: string) {
+        setTasks({...tasks, [todoId]: [{id: v1(), title, isDone: false},...tasks[todoId]]});
 
-    let [title1, setTitle1] = useState<string>('')
-
-    const addMessage1 = () => {
-        setMessage1([{message: title1}, ...message1])
-        setTitle1('')
+        // let task = {id: v1(), title: title, isDone: false};
+        // let newTasks = [task, ...tasks];
+        // setTasks(newTasks);
     }
+
+    function changeStatus(todoId: string, taskId: string, isDone: boolean) {
+
+        setTasks({...tasks, [todoId]: tasks[todoId].map(t => t.id === taskId ? {...t, isDone} : t)});
+
+        // let task = tasks.find(t => t.id === taskId);
+        // if (task) {
+        //     task.isDone = isDone;
+        // }
+        //
+        // setTasks([...tasks]);
+    }
+
+
+    let tasksForTodolist = tasks;
+
+
+
+    function changeFilter( todoListId: string, value: FilterValuesType) {
+        setTodolists(todolists.map(toDo => toDo.id === todoListId ? {...toDo, filter: value} : toDo))
+    }
+
 
     return (
-        <>
+        <div className="App">
+            {todolists.map((toDo => {
+                let tasksForToDo = tasks[toDo.id]
+                if (toDo.filter === "active") {
+                    tasksForToDo = tasks[toDo.id].filter(t => t.isDone === false);
+                }
+                if (toDo.filter === "completed") {
+                    tasksForToDo = tasks[toDo.id].filter(t => t.isDone === true);
+                }
+                return (<Todolist
+                            key={toDo.id}
+                            todoListId={toDo.id}
+                            title = {toDo.title}
+                            tasks = {tasksForToDo}
+                            removeTask={(taskId) => removeTask(toDo.id, taskId)}
+                            changeFilter={(value: FilterValuesType)=>changeFilter(toDo.id, value)}
+                            addTask={(title)=> addTask(toDo.id, title)}
+                            changeTaskStatus={(taskId: string, isDone: boolean)=> changeStatus(toDo.id,taskId, isDone)}
+                            filter={toDo.filter}
+                />)
+            }))}
 
-            <>
-                <Task day={'MONDAY'} task={1}/>
-                <Header titleHeader={'Header-very good'}/>
-                <Body titleBody={'Body-very well'}/>
-                <Footer titleFooter={'Footer-it is beautiful'}/>
-            </>
-            <br/>
-            <>
-                <Task day={'MONDAY'} task={'2-3'}/>
-                <NewComponent students={students}/>
-                <br/>
-                <CarsTable topCars={topCars}/>
-                <Button name={'MyYouTubeChannel-1'} callback={() => Button1foo('I m Vasya', 21)}/>
-                <Button name={'MyYouTubeChannel-2'} callback={() => Button2foo('I m Ivan', 30)}/>
-                <Button name={'Stupid Button'} callback={Button3foo}/>
-            </>
-            <br/>
-            <br/>
-            <>
-                <Task day={'MONDAY'} task={4}/>
-                <h1>{a}</h1>
-                <Button name={'УВЕЛИЧИТЬ ЧИСЛО'} callback={counterButton}></Button>
-                <Button name={'ОБНУЛИТЬ СЧЕТЧИК'} callback={resetCounterButton}></Button>
-            </>
-            <br/>
-            <br/>
-            <>
-                <Task day={'MONDAY'} task={5}/>
-                <h3>МЕТОД FILTER</h3>
-                <Money money={filteredMoney}/>
-                <div style={{marginLeft: '60px'}}>
-                    <Button name={'allMoney'} callback={() => moneyFilterHandler('allMoney')}></Button>
-                    <Button name={'RUBLS'} callback={() => moneyFilterHandler('RUBLS')}></Button>
-                    <Button name={'Dollars'} callback={() => moneyFilterHandler('Dollars')}></Button>
-                </div>
-                <br/>
-            </>
-            <>
-                <Task day={'MONDAY'} task={6}/>
-                <h3>INPUT</h3>
-                <h3>FULLINPUT</h3>
-                <Fullinput addMessage={addMessage}/>
-                {message.map((m, i) => <div key={i + 1}>{m.message}</div>)}
-                <br/>
-                <h3>INPUT + BUTTON</h3>
-                <Input title={title1} setTitle={setTitle1}/>
-                <ButtonInput name={'+'} callback={addMessage1}/>
-                {message1.map((m, i) => <div key={i + 1}>{m.message}</div>)}
-
-
-            </>
-
-        </>
+        </div>
     );
 }
 
